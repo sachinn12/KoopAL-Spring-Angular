3480a18ebf674d21401b41f412f5b4f37956f846
sachinn12 <sachindv42@gmail.com>
2023-08-20 22:21:59 +0200

    linking project with git

---
+++ src/main/java/com/example/koopal/KoopAlApplication.java
@@ -0,0 +1,13 @@
+package com.example.koopal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoopAlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoopAlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/koopal/KoopAlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koopal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoopAlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
